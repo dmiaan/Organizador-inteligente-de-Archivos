db75344fda8f8393b0ecff73a01ec3b6a4318217
dmiann <damiancito260164@gmail.com>
2026-08-17 23:01:58 -0400

    Creación de script en Python para organización automática de archivos

---
+++ README.md
@@ -0,0 +1,34 @@
+📁Organizador Inteligente de Archivos (Script de Automatización)
+
+- Un script eficiente desarrollado en Python para ordenar cualquier carpeta automáticamente. Le indicas la ruta del directorio que quieres organizar por la terminal, y él se encarga de acomodar cada archivo en su respectiva categoría según su formato.
+
+✱ 𝐂𝐚𝐫𝐚𝐜𝐭𝐞𝐫𝐢́𝐬𝐭𝐢𝐜𝐚𝐬
+
+● Es interactivo: Pide por consola la ruta exacta de la carpeta que deseas ordenar.
+
+● Clasificación: Organiza documentos, imágenes, ejecutables, audios, videos, comprimidos, código y más.
+
+● Es seguro: Si la carpeta de destino ya existe, la usa directamente; si no, la crea sin romper nada.
+
+● Ignora mayúsculas: No importa si la extensión está en mayúsculas, igualmente la procesa.
+
+✱ 𝐓𝐞𝐜𝐧𝐨𝐥𝐨𝐠𝐢́𝐚𝐬 𝐲 𝐋𝐢𝐛𝐫𝐞𝐫𝐢́a𝐬 𝐮𝐭𝐢𝐥𝐢𝐳𝐚𝐝𝐚𝐬:
+
+● Python 3
+
+● Librerías Nativas: 
+
+os: Librería que nos permite conectar el código con nuestro sistema operativo.
+
+shutil: Esta librería nos permite realizar operaciones de alto nivel en archivos y directorios.
+
+✱ ¿𝐂𝐨́𝐦𝐨 𝐮𝐬𝐚𝐫𝐥o?:
+
+1. Clona o descarga este repositorio.
+2. Abre tu terminal en la carpeta del proyecto.
+3. Ejecuta el script:
+   ```bash
+   python organizador.py
+4. Escribe o pega la ruta de la carpeta que deseas ordenar cuando te lo pida, ¡y listo!   
+
+
---
+++ main.py
@@ -0,0 +1,39 @@
+
+import os 
+import shutil 
+
+carpetas = {"Ejecutables": [".exe"],
+            "Imágenes": [".jpg", ".jpeg", ".png", ".gif", ".svg", ".webp"],
+            "Documentos": [".docx", ".doc", ".pdf", ".txt", ".xlsx", ".xls", ".pptx", ".ppt"],
+            "Archivos comprimidos": [".zip", ".rar", ".7z", ".tar", ".gz"],
+            "Multimedia - Audio": [".mp3", ".wav", ".aac", ".flac", ".ogg"],
+            "Multimedia - Videos": [".mp4", ".mkv", ".avi", ".mov", ".wmv"],
+            "Desarrollo / Código": [".py", ".js", ".html", ".css", ".json", ".sql"],
+            "Instaladores / Imágenes de Disco": [".msi", ".dmg", ".iso", ".pkg"],
+            "Diseño / Gráficos Avanzados": [".psd", ".ai"]}
+
+ruta_carpeta = input("Introduce la ruta de la carpeta que deseas organizar: ") #  Pidiendole al usuario que introduzca la ruta de la carpeta. 
+
+
+for elemento in os.listdir(ruta_carpeta): # Recorriendo los archivos y carpetas del directorio  
+    ruta_completa = os.path.join(ruta_carpeta, elemento) 
+    if os.path.isfile(ruta_completa): # El filtro de seguridad para comprobar si es un archivo o una carpeta
+        print(f"Archivo detectado: {elemento}")
+        nombre, extension = os.path.splitext(elemento) # Separación del texto
+        extension = extension.lower()
+        destino = "Otros" # Asignando un destino por defecto
+        for nombre_categoria, lista_extensiones in carpetas.items():
+            if extension in lista_extensiones:
+                destino = nombre_categoria
+                break
+        ruta_exacta = os.path.join(ruta_carpeta, destino)  
+        if not os.path.isdir(ruta_exacta):
+            os.mkdir(ruta_exacta) # Creamos la carpeta si no existe 
+        ruta_final = os.path.join(ruta_exacta, elemento)     
+        archivo_mudanza = shutil.move(ruta_completa, ruta_final)  # Haciendo la mudanza del archivo         
+                
+   
+   
+            
+    
+
